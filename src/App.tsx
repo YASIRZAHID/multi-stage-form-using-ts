0721cc4f77d6b280b18cad16cdf0df3cf8cb1881
YASIR ZAHID <science5661@gmail.com>
2023-10-23 03:59:42 +0500

    working ts form

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import SignupPage2 from "./components/Step2";
+import SignupPage from "./components/Step1";
+import DisplayFormValues from "./components/Step3";
+import React from "react";
+type FormData = {
+  username?:string,
+  email?:string,
+  phoneNumber?:string,
+  country?:string,
+  password?:string
+};
 
 function App() {
+  const [step, setStep] = useState(1); // Initialize with the first step
+
+  const nextStep = () => {
+    setStep(step + 1);
+  };
+  const data:FormData={};
+  const [formData, setFormData] = useState(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App min-h-screen ">
+      <div>
+        {step === 1 && (
+          <SignupPage
+            nextStep={nextStep}
+            formData={formData}
+            setFormData={setFormData}
+          />
+        )}
+        {step === 2 && (
+          <SignupPage2
+            nextStep={nextStep}
+            formData={formData}
+            setFormData={setFormData}
+          />
+        )}
+        {step === 3 && <DisplayFormValues formData={formData} />}
+      </div>
     </div>
   );
 }
